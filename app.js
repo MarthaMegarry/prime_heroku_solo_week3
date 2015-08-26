130ca062826a4510c24f80959884269303d5315b
MarthaMegarry <mega0015@umn.edu>
2015-08-25 23:26:01 -0500

    got express called correctly and uploaded my resume

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-var http = require('http');
+//var http = require('http');
 var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+var server = app.listen(process.env.PORT || 3000, function() {
+    var port = server.address().port;
+    console.log('Listening on port: ', port);
+});
 
 
-Express.createServer(function(req, res){
-    res.writeHead(200);
-    res.write("Hello world");
-    res.end();
-}).listen(process.env.PORT || 3000);
+
+////express(function(req, res){
+//    res.writeHead(200);
+//    res.write("Hello world");
+//    res.end();
+//}).listen(process.env.PORT || 3000);
